2a20c9f48b6704102468a41a93b289962fd0e501
hitesharora1997 <hitesh.arora1997@gmail.com>
2024-01-06 01:34:42 +0100

    add a display function

--- todo/src/lib.rs
+++ todo/src/lib.rs
@@ -1,13 +1,33 @@
 #[warn(dead_code)]
+#[derive(Debug)]
 pub struct Task {
     task_Id: u32,
     task: String,
     done_Status: bool,
 }
 
+fn add_new_task(t: &mut Vec<Task>, nTask: &str) {
+    unimplemented!()
+}
+
+fn display_todo(t: &mut Vec<Task>) {
+    println!("{:?}", t);
+}
+
 fn parse_arg(arg: Vec<&str>, t: &mut Vec<Task>) {
     let command = arg[0];
-    println!("{}", command);
+
+    match command {
+        "add" => {
+            if let Some(val) = arg.get(1) {
+                let new_task = *val;
+                // Adding the todo list with the new task
+                add_new_task(t, new_task);
+                display_todo(t);
+            }
+        }
+        _ => println!("empty command"),
+    };
 }
 
 pub fn run(arg: Vec<&str>, t: &mut Vec<Task>) {
